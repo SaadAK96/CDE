eead5c777e83bce227f23d0c717533b3b8f55eca
Saad <saadahmed.aqueel@gmail.com>
2024-01-13 19:37:02 +0500

    class 1

---
+++ 1_HelloWorld.py
@@ -0,0 +1,14 @@
+print('Hello World')
+
+fav_lang = {
+    'jen' : ['python','java'],
+    'sarah' : ['js'],
+    'edward' : ['ruby']
+}
+
+for i,j in fav_lang.items():   print(f"the language of {i} is")
+    for k in j:
+    
+
+        print(j[i])
+    
